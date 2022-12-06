63a5234fc20c53eadd07b5f6593e220831cdcf7a
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2022-12-06 03:39:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+
+/*Return the element "Donatello".
+
+ Remember the first element in an array is 0.
+ Always return your answer!*/
+
+
+const turtles = ["Raphael", "Michelangelo", "Leonardo", "Donatello"]
+
+function turtlePower(turtles) {
+	 
+  return turtles[3]
+  
+}
+ 
